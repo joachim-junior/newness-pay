9a567b145f2439c56bf123544dafad7d9c313dde
Joachim Junior <you@example.com>
2021-02-05 14:52:03 +0100

    Simple initialization of frontend and backend

---
+++ frontend/src/views/reports/DashboardView/index.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import {
+  Container,
+  Grid,
+  makeStyles
+} from '@material-ui/core';
+import Page from 'src/components/Page';
+import Budget from './Budget';
+// import LatestOrders from './LatestOrders';
+// import LatestProducts from './LatestProducts';
+// import Sales from './Sales';
+import TasksProgress from './TasksProgress';
+import TotalCustomers from './TotalCustomers';
+import TotalProfit from './TotalProfit';
+// import TrafficByDevice from './TrafficByDevice';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.dark,
+    minHeight: '100%',
+    paddingBottom: theme.spacing(3),
+    paddingTop: theme.spacing(3)
+  }
+}));
+
+const Dashboard = () => {
+  const classes = useStyles();
+
+  return (
+    <Page
+      className={classes.root}
+      title="Dashboard"
+    >
+      <Container maxWidth={false}>
+        <Grid
+          container
+          spacing={3}
+        >
+          <Grid
+            item
+            lg={3}
+            sm={6}
+            xl={3}
+            xs={12}
+          >
+            <Budget />
+          </Grid>
+          <Grid
+            item
+            lg={3}
+            sm={6}
+            xl={3}
+            xs={12}
+          >
+            <TotalCustomers />
+          </Grid>
+          <Grid
+            item
+            lg={3}
+            sm={6}
+            xl={3}
+            xs={12}
+          >
+            <TasksProgress />
+          </Grid>
+          <Grid
+            item
+            lg={3}
+            sm={6}
+            xl={3}
+            xs={12}
+          >
+            <TotalProfit />
+          </Grid>
+        </Grid>
+      </Container>
+    </Page>
+  );
+};
+
+export default Dashboard;
